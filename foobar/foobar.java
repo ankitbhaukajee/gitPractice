9aa54b45987f4b3db5e2815377f0d24f3b8e86d3
AnkitBhaukajee <ankitbhaukajee@gmail.com>
2018-08-07 15:47:12 -0400

    new file added

---
+++ foobar/foobar.java
@@ -0,0 +1,23 @@
+package masud;
+
+
+public class foobar {
+
+	public static void main(String[] args) {
+		for (int i=1;i<=50;i++){
+			if (i%3==0 && i%5==0){
+				System.out.println("foobar");
+			}
+			else if (i%5==0){
+				System.out.println("bar");
+			}
+			else if (i%3==0){
+				System.out.println("foo");
+			}
+			else
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
